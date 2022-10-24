89c453764698b75505c886752ddabd08401512ac
sakib5452 <najmus191@gmail.com>
2022-10-25 05:44:58 +0600

    listen

--- index.js
+++ index.js
@@ -2,3 +2,20 @@ const express = require ('express');
 const app = express();
 const cors = require('cors');
 app.use(cors());
+const Port = process.env.Port || 5000;
+
+const coursesCollection = require("./course.json");
+
+app.get("/", (req, res) =>{
+    res.send ("Now server is running;")
+});
+
+app.get('/allCourses', (req, res) => {
+ 
+    res.send(coursesCollection);
+
+});
+
+app.listen(Port, ()=>{
+    console.log("server is running", Port);
+});
